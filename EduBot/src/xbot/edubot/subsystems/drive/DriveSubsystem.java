0919971102dc013b9908675f5a5856c8c77e23f3
John Li <john.li9631@gmail.com>
2018-01-13 11:38:30 -0800

    TankDrive

--- EduBot/src/xbot/edubot/subsystems/drive/DriveSubsystem.java
+++ EduBot/src/xbot/edubot/subsystems/drive/DriveSubsystem.java
@@ -40,5 +40,8 @@ public class DriveSubsystem {
 		// an example, here is some code that has the frontLeft motor to spin according to
 		// the value of leftPower:
 		frontLeft.set(leftPower);
+		frontRight.set(rightPower);
+		rearLeft.set(leftPower);
+		rearRight.set(rightPower);
 	}
 }
